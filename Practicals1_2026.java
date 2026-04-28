95d687f061f0de60b4db3088fdb07065c265c364
25019625 <25019625@mvula.univen.ac.za>
2026-04-28 16:25:13 +0200

    First Commit

---
+++ Practicals1_2026.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicals1_2026;
+
+/**
+ *
+ * @author ramas
+ */
+public class Practicals1_2026 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame j=new NewJFrame();
+        j.setVisible(true);
+    }
+    
+}
